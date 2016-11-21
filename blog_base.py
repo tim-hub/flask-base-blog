4458bc594d5af2e882f8f306503786c37db64a62
tim-hub <tim_w@outlook.com>
2016-11-21 13:01:44 +1300

    add mui css

--- blog_base.py
+++ blog_base.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
+from werkzeug.security import generate_password_hash, \
+     check_password_hash
 import logging
 from google.appengine.ext import db
 
+'''
+This is google datastore model to store post
+'''
 class BlogPost(db.Model):
-	subject= db.StringProperty(required=True)
-	content=db.TextProperty(required=True)
-	created=db.DateTimeProperty(auto_now_add=True)
+    subject= db.StringProperty(required=True)
+    content=db.TextProperty(required=True)
     modified=db.DateTimeProperty(auto_now=True)
+    created=db.DateTimeProperty(auto_now_add=True)
+
+
 app = Flask(__name__)
+
+def encrypt_val(s):
+    return generate_password_hash(s)
+
+def check_val(h,s):
+    return check_password_hash(h,s)
 
 @app.route('/')
 def home():
@@ -40,8 +53,43 @@ def show_post(post_id):
     blog_post=BlogPost.get_by_id(long(post_id))
     return render_template('post.html', subject=blog_post.subject, content=blog_post.content)
 
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method=='GET':
+        return ("sign up")
+    else:
+        pass
+
+
 if __name__ == '__main__':
     app.run()
+
+
+
+'''
+this is only for test
+'''
+@app.route('/test-cookie', methods=['GET', 'POST'])
+def test_cookie():
+    if request.method=='POST':
+        pass
+    else:
+        visit_times = request.cookies.get('visits','0')
+        if visit_times.isdigit():
+            visit_times = int(visit_times) + 1
+        else:
+            visit_times=0
+
+        if visit_times>500:
+            resp = make_response(render_template('space.html', str='it is good'))
+            resp.set_cookie('visits', str(visit_times))
+        else:
+            resp = make_response(render_template('space.html', str='you visit %s times' %visit_times))
+            resp.set_cookie('visits', str(visit_times))
+
+        return resp
 
 @app.errorhandler(500)
 def server_error(e):
